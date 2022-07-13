d7f41693b2825efe1ba10ab1b4f0e2e527b8b5ed
gingmzmzx <2417481092@qq.com>
2022-07-14 06:33:44 +0800

    更改热搜列表获取方式，不再依赖NeteaseCloudMusicApi

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-import sys, requests
+import sys, requests, re, json
+import urllib.request
+import urllib.error
+import urllib.parse
 from pyncm import apis
 sys.path.append('../..')
 import go
+
+def get_all_hotSong():  # 获取热歌榜所有歌曲名称和id
+    url = 'http://music.163.com/discover/toplist?id=3778678'  # 网易云云音乐热歌榜url
+    header = {  # 请求头部
+        'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    request = urllib.request.Request(url=url, headers=header)
+    html = urllib.request.urlopen(request).read().decode('utf8')  # 打开url
+    html = str(html)  # 转换成str
+    pat1 = r'<ul class="f-hide"><li><a href="/song\?id=\d*?">.*</a></li></ul>'  # 进行第一次筛选的正则表达式
+    result = re.compile(pat1).findall(html)  # 用正则表达式进行筛选
+    result = result[0]  # 获取tuple的第一个元素
+
+    pat2 = r'<li><a href="/song\?id=\d*?">(.*?)</a></li>'  # 进行歌名筛选的正则表达式
+    pat3 = r'<li><a href="/song\?id=(\d*?)">.*?</a></li>'  # 进行歌ID筛选的正则表达式
+    hot_song_name = re.compile(pat2).findall(result)  # 获取所有热门歌曲名称
+    hot_song_id = re.compile(pat3).findall(result)  # 获取所有热门歌曲对应的Id
+
+    return hot_song_name, hot_song_id
+
 
 def music_hot_search(meta_data):
     uid = meta_data.get('se').get('user_id')
     gid = meta_data.get('se').get('group_id')
     message = meta_data.get('message')
     
-    data = requests.get(meta_data.get('botSettings').get('musicApi')+'search/hot/detail').json().get('data')
+    # data = apis.playlist.GetTopPlaylists()
+    # data = requests.get(meta_data.get('botSettings').get('musicApi')+'search/hot/detail').json().get('data')
+    data = get_all_hotSong()
     message = '[CQ:face,id=189] 网易云热搜列表：'
-    for i in data:
-        message += '\n[CQ:face,id=161] 搜索词：'+str(i.get('searchWord'))+'\n     搜索次数：'+str(i.get('score'))
+    
+    limit = meta_data.get('message') or 10
+    i = 0
+    while i < int(limit):
+        message += '\n[CQ:face,id=161] 歌曲名：'+str(data[0][i])+'\n     歌曲ID：'+str(data[1][i])
+        i += 1
     go.send(meta_data, message)
 
 def play_music(meta_data):
